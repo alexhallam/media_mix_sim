5d20c01f995a90d152cd00a005eefc4e9528eec7
Hallam <AHallam@Arbys.com>
2018-05-22 15:33:38 -0400

    add_simple_example

---
+++ simple_example.R
@@ -0,0 +1,20 @@
+library(rstan)
+library(tidyverse)
+
+clean_data <- read_csv("clean_data.csv")
+media_data <- clean_data %>% select(contains("m_"))
+# data Prep
+N <- nrow(clean_data)
+Y <- clean_data$sales
+max_lag <- 13
+num_media <- 3
+lag_vec <- seq(0, max_lag - 1)
+X_media <- array(data = media_data, dim = c(3,13))
+num_ctrl <- 1
+X_ctrl <- clean_data$price
+
+stan_data <- list(N=N, Y=Y, max_lag=max_lag, num_media=num_media,
+                  lag_vec=lag_vec,X_media=X_media,
+                  num_ctrl=num_ctrl,X_ctrl=X_ctrl)
+
+m.stan <- stan(file = "model.stan",data = stan_data, iter = 500, chains = 4)
